42bd25955f363fd7c315f3700a3cd902d13394c5
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2024-09-08 14:21:42 -0500

    🔧 Add(pyproject.toml): Install essential libraries, configure settings, and define project description

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-from fastapi import FastAPI
+"""
+The main script that initiates and runs the FastAPI application.
+This module sets up the application configuration.
+"""
 
-app = FastAPI()
+import uvicorn
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse, UJSONResponse
+from starlette.middleware.gzip import GZipMiddleware
+
+app: FastAPI = FastAPI(
+    default_response_class=UJSONResponse,
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get(
+    "/",
+    status_code=status.HTTP_307_TEMPORARY_REDIRECT,
+    include_in_schema=False,
+    response_class=RedirectResponse,
+)
+async def redirect_to_docs() -> RedirectResponse:
+    """
+    Redirects the user to the /docs endpoint for API documentation.
+
+    ## Response:
+    - `return:` **The redirected response**
+    - `rtype:` **RedirectResponse**
+    """
+    return RedirectResponse("/docs")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        reload=True,
+    )
